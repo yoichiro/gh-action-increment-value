a9d8b271cdc17025442a66b969fecd917d2d4e2d
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-02-19 17:32:06 +0900

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:14.14-slim
+
+LABEL "com.github.actions.name"="Automated value increment."
+LABEL "com.github.actions.description"="Automated value increment."
+LABEL "com.github.actions.icon"="plus"
+LABEL "com.github.actions.color"="green"
+
+COPY package*.json ./
+
+RUN apt-get update
+RUN apt-get -y install git
+RUN npm ci --only=production
+
+COPY . .
+
+ENTRYPOINT ["node", "/index.js"]
---
+++ index.js
@@ -0,0 +1,47 @@
+const { Toolkit } = require('actions-toolkit');
+const fs = require('fs');
+
+if (process.env.INPUT_TARGET_DIRECTORY) {
+  process.env.GITHUB_WORKSPACE = `${process.env.GITHUB_WORKSPACE}/${process.env.INPUT_TARGET_DIRECTORY}`;
+  process.chdir(process.env.GITHUB_WORKSPACE);
+}
+
+Toolkit.run(async tools => {
+  try {
+    // Read the target file
+    const targetFile = process.env.INPUT_TARGET_FILE;
+    const content = fs.readFileSync(targetFile, 'utf8');
+    // Increment value
+    const prefix = process.env.INPUT_PREFIX;
+    const suffix = process.env.INPUT_SUFFIX;
+    const firstPart = content.substring(0, content.indexOf(prefix) + prefix.length);
+    const lastPart = content.substring(content.indexOf(suffix));
+    const targetPart = content.substring(content.indexOf(prefix) + prefix.length, content.indexOf(suffix));
+    const current = Number(targetPart);
+    const next = current + 1;
+    // Write the target file
+    const newContent = `${firstPart}${String(next)}${lastPart}`;
+    fs.writeFileSync(targetFile, newContent);
+    console.log(`Incremented the value from ${current} to ${next}.`);
+    // Set git user
+    await tools.runInWorkspace('git', ['config', 'user.name', `"${process.env.GITHUB_USER || 'Automated Increment value'}"`]);
+    await tools.runInWorkspace('git', ['config', 'user.email', `"${process.env.GITHUB_EMAIL} || 'gh-action-increment-value@users.noreply.github.com"`]);
+    // Fetch current branch name
+    let currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1];
+    let isPullRequest = false;
+    if (process.env.GITHUB_HEAD_REF) {
+      currentBranch = process.env.GITHUB_HEAD_REF;
+      isPullRequest = true;
+    }
+    console.log(`Current branch: ${currentBranch}`);
+    // Commit
+    await tools.runInWorkspace('git', ['commit', '-a', '-m', `ci: Increment the value to ${next}`]);
+    // Push
+    const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPORISORY}.git`;
+    await tools.runInWorkspace('git', ['push', remoteRepo]);
+    tools.exit.success('Incrementing the value successfully.');
+  } catch(e) {
+    tools.log(e);
+    tools.exit.failure('Failed to increment the value.');
+  }
+});
